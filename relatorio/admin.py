63f5443ba3d4532020df7a2e909b132354e876cb
Guilherme <55714002+littlegui2@users.noreply.github.com>
2022-11-02 10:38:29 -0500

    update

--- relatorio/admin.py
+++ relatorio/admin.py
@@ -2,11 +2,8 @@ from django.contrib import admin
 from .models import Relatorio, Equipe
 # Register your models here.
 
-class RelatorioAdmin(admin.ModelAdmin):
-    list_display = ('codigo', 'frequencia','data','mes','data_criacao')
-     
-    search_fields = ("mes",)
 
 
-admin.site.register(Relatorio,RelatorioAdmin)
+
+admin.site.register(Relatorio)
 admin.site.register(Equipe)
